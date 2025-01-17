c1992f08a2fc013bf458a50bcd292ab5ced9713d
osilove <167946093+osilove@users.noreply.github.com>
2025-01-17 10:01:59 +0900

    Add files via upload

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,33 @@
 class SchedulesController < ApplicationController
-  def index
+  before_action :authenticate_user!
+
+  def create
+    @schedule = current_user.schedules.new(schedule_params)
+    if @schedule.save
+      redirect_to schedules_path, notice: '予定を追加しました。'
+    else
+      render :index
+    end
+  end
+
+  def update
+    @schedule = current_user.schedules.find(params[:id])
+    if @schedule.update(schedule_params)
+      redirect_to schedules_path, notice: '予定を更新しました。'
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @schedule = current_user.schedules.find(params[:id])
+    @schedule.destroy
+    redirect_to schedules_path, notice: '予定を削除しました。'
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:date, :text, :start_time, :end_time, :important)
   end
 end
