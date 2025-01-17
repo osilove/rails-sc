a10aa47cbc86b14e62f1b2bc76030e8928afe707
osilove <167946093+osilove@users.noreply.github.com>
2025-01-17 11:08:39 +0900

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # devise_for :users を1回だけ定義します
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'  # ユーザー登録をカスタマイズしたい場合はここを指定
+  }
+
+  # スケジュールのルーティング
+  resources :schedules, only: [:index, :create, :update, :destroy]
+
+  # ルートページをスケジュール一覧ページに設定
   root 'schedules#index'
-  end
+end
